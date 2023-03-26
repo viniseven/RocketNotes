36635ef0fe61308f074304be709f5bd9b5d7d9a5
Marcus Vinicius <marcusv892@gmail.com>
2023-03-25 23:32:34 -0300

    Insert hooks, for authentication user, and context in between routes

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,13 +1,12 @@
 import { Container } from "./styles";
 
 
-export function Button({ title, loading = false}){
-    return(
-        <Container 
-            type="button"
-            disabled={loading}
-        >
-            {loading ? "Carregando..." : title }
+
+
+export function Button({ title, ...rest }) {
+    return (
+        <Container type="button" {...rest}>
+            {title}
         </Container>
-    )
+    );
 }
