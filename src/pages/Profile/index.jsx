dff76d4181bd969ec22543232d748d6a4cdb305b
Marcus Vinicius <marcusv892@gmail.com>
2023-03-26 01:59:58 -0300

    Inserting authentication and data user in localstorage for persist data in application

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,12 +1,35 @@
+import { useState } from 'react';
+
 import { Container, Form, Avatar} from './styles'
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 
 import { Link } from 'react-router-dom';
 
+import { useAuth } from '../../hooks/auth';
+
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
 export function Profile(){
+
+    const { user, updateProfile } = useAuth();
+
+    const [name, setName] = useState(user.name);
+    const [email, setEmail] = useState(user.email);
+    const [passwordOld, setPasswordOld] = useState();
+    const [passwordNew, setPasswordNew] = useState();
+
+    async function handleUpdate(){
+        const user = {
+            name,
+            email,
+            password: passwordNew,
+            old_password: passwordOld
+        }
+        
+        await updateProfile({ user })
+    }
+
     return(
         <Container>
                 <header>
@@ -35,27 +58,33 @@ export function Profile(){
                         placeholder="Nome"
                         type="text"
                         icon={FiUser}
+                        value={name}
+                        onChange = {e => setName(e.target.value)}
                     />
 
                     <Input 
                         placeholder="Email"
                         type="text"
                         icon={FiMail}
+                        value={email}
+                        onChange = {e => setEmail(e.target.value)}
                     />
 
                     <Input 
                         placeholder="Senha atual"
                         type="password"
                         icon={FiLock}
+                        onChange = {e => setPasswordOld(e.target.value)}
                     />
 
                     <Input 
                         placeholder="Nova senha"
                         type="password"
                         icon={FiLock}
+                        onChange = {e => setPasswordNew(e.target.value)}
                     />
 
-                    <Button title="Salvar" />
+                    <Button title="Salvar" onClick={handleUpdate} />
                 </Form>
         </Container>
     )
